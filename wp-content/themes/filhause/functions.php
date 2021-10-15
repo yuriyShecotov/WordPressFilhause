b231f238a9899fc55e2b6a6f1e88db598df03d1d
Schekotov <schekotov@outlook.com>
2021-10-15 19:00:16 +0300

    v.0.9.2

--- wp-content/themes/filhause/functions.php
+++ wp-content/themes/filhause/functions.php
@@ -178,14 +178,54 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
- if( function_exists('acf_add_options_page') ) {
+if( function_exists('acf_add_options_page') ) {
 
-        // Register options page.
-        $option_page = acf_add_options_page(array(
-            'page_title'    => __('Настройка шапки и подвала'),
-            'menu_title'    => __('Настройки сайта'),
-            'menu_slug'     => 'theme-general-settings',
-            'capability'    => 'edit_posts',
-            'redirect'      => false
-        ));
-    }
+	// Register options page.
+	$option_page = acf_add_options_page(array(
+		'page_title'    => __('Настройка шапки и подвала'),
+		'menu_title'    => __('Настройки сайта'),
+		'menu_slug'     => 'theme-general-settings',
+		'capability'    => 'edit_posts',
+		'redirect'      => false
+	));
+}
+add_action( 'init', 'register_post_types' );
+function register_post_types(){
+	register_post_type( 'post_type_name', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Дома', // основное название для типа записи
+			'singular_name'      => 'дом', // название для одной записи этого типа
+			'add_new'            => 'Добавить дом', // для добавления новой записи
+			'add_new_item'       => 'Добавление домов', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование домов', // для редактирования типа записи
+			'new_item'           => 'Новый дом', // текст новой записи
+			'view_item'          => 'Смотреть дом', // для просмотра записи этого типа.
+			'search_items'       => 'Искать дом', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Дома', // название меню
+		],
+		'description'         => '',
+		'public'              => true,
+		'publicly_queryable'  => true, // зависит от public
+		'exclude_from_search' => true, // зависит от public
+		'show_ui'             => true, // зависит от public
+		'show_in_nav_menus'   => true, // зависит от public
+		'show_in_menu'        => true, // показывать ли в меню адмнки
+		'show_in_admin_bar'   => true, // зависит от show_in_menu
+		'show_in_rest'        => true, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => 4,
+		'menu_icon'           => null,
+		//'capability_type'   => 'post',
+		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+		'hierarchical'        => false,
+		'supports'            => [ 'title', 'editor','page-attributes' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'taxonomies'          => [],
+		'has_archive'         => false,
+		'rewrite'             => true,
+		]);
+	}
